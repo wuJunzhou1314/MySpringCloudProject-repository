466e87685ca3238b8ca39cdf06090d4f94d305c1
wjz826 <15135358163@sina.cn>
2021-08-29 14:23:25 +0800

    注释说明当前主程序的需要的插件

--- SC1802-project-authentication-consumer-4000/src/main/java/com/zpark/sc/AppMainClass.java
+++ SC1802-project-authentication-consumer-4000/src/main/java/com/zpark/sc/AppMainClass.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+/**
+ * 	这是主启动类：
+ * 		加入远程连接接口
+ * 		并且把程序注册到Eureka注册中心
+ * @author lenovo
+ *
+ */
 @EnableFeignClients //添加Feign支持
 @SpringBootApplication
 @EnableEurekaClient
